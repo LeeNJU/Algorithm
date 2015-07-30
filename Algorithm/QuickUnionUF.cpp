e7f5c9235f02c2ff67b42d8d45edfb823dbabcf3
LeeNJU <lhb11@software.nju.edu.cn>
2015-07-30 18:38:02 -0400

    WeightedQuickUnion

--- Algorithm/QuickUnionUF.cpp
+++ Algorithm/QuickUnionUF.cpp
@@ -31,3 +31,52 @@ public:
 		parent[i] = j;
 	}
 };
+
+
+//上面的QuickUnion方法在执行union操作的时候，有时候可能会把高度大的树合并到高度小的树的下面，这样导致树不平衡
+//WeightedQuickUnion有改进，用一个size数组记录每个子集的元素个数，unino操作在合并的时候，总是把高度小的树连接到高度
+//大的树下面
+class WeightedQuickUnionUF
+{
+private:
+	std::vector<int> parent;
+	std::vector<int> size;  //size[i]表示以i为根节点的树中节点数量
+	int root(int i)  //求出元素i所在的树的根节点
+	{
+		while (i != parent[i])
+			i = parent[i];
+		return i;
+	}
+public:
+	WeightedQuickUnionUF(int N)
+	{
+		for (int i = 0; i < N; i++) //初始化
+		{
+			parent.push_back(i);
+			size[i] = 1;
+		}
+	}
+
+	bool connected(int p, int q) //通过比较p和q的根节点是否相同来判断p和q是否在同一个子集（树）中
+	{
+		return root(p) == root(q);
+	}
+
+	void unions(int p, int q) //先求出p和q的子集（树）的根节点，把p的根节点的父节点改为q的根节点
+	{
+		int i = root(p);
+		int j = root(q);
+		if (i == j)
+			return;
+		if (size[i] < size[j])
+		{
+			parent[i] = j;
+			size[j] += size[i];
+		}
+		else
+		{
+			parent[j] = i;
+			size[i] += size[j];
+		}
+	}
+};
